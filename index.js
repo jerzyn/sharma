b58069947640ac4f5656af40134c31fecf7cad1a
jerzyn <andrew.jarzyna@gmail.com>
2015-11-11 12:43:39 +0100

    Clean js code

--- index.js
+++ index.js
@@ -13,45 +13,43 @@ app.post('/pre-register', function (req, res) {
   //     body: 'node rocks!'
   //   }
   // ])
-	var first_name = req.body.first_name;
-	var last_name = req.body.last_name;
-	var birthdate = req.body.birthdate;
-	var email = req.body.email;
-	var status = req.body.status.replace(/\s/g,'');
-	var street = req.body.street;
-	var postcode = req.body.postcode;
-	var city = req.body.city;
-	var telephone = req.body.telephone;
+  'use strict';
+  var first_name = req.body.first_name;
+  var last_name = req.body.last_name;
+  var birthdate = req.body.birthdate;
+  var email = req.body.email;
+  var status = req.body.status.replace(/\s/g,'');
+  var street = req.body.street;
+  var postcode = req.body.postcode;
+  var city = req.body.city;
+  var telephone = req.body.telephone;
   // Normal;;LAST NAME;NAME;DATEOFBIRTH(DDMMYYY);STREET;CODIGOPOSSTAL;PHONENUMBER;MAIL
-	var user = status + ';' + last_name  + ';' + first_name + ';' + birthdate + ';' +  street + ';' + postcode + ';' + telephone + ';' + city + ';' + email + '\n';
+  var user = status + ';' + last_name  + ';' + first_name + ';' + birthdate + ';' +  street + ';' + postcode + ';' + telephone + ';' + city + ';' + email + '\n';
   fs.readFile('./register.csv', function (err, data) {
-  	if (err) {
-	  	fs.writeFile('register.csv', user , function (err) {
+    if (err) {
+      fs.writeFile('register.csv', user , function (err) {
         if (err) {
           console.log(err);
           return res.sendStatus(err.status);
         }
       });
-  	} else {
+    } else {
       fs.appendFile('register.csv', user , function (err) {
         if (err) {
           console.log(err);
           return res.sendStatus(err.status);
         }
-      });  	  
-  	}
+      });
+    }
   });
   res.sendStatus(201);
 });
 
 app.get('/register.csv', function (req, res) {
+  'use strict';
   var options = {
-    root: __dirname + '/',
-    // headers: {
-    //     'x-timestamp': Date.now(),
-    //     'x-sent': true
-    // }
-  };	
+    root: __dirname + '/'
+  };
   res.sendFile('register.csv', options, function (err) {
     if (err) {
       console.log(err);
@@ -68,5 +66,5 @@ app.get('/register.csv', function (req, res) {
 
 
 app.listen(3000, function () {
-  console.log('Server listening on', 3000)
-})
+  console.log('Server listening on', 3000);
+});
