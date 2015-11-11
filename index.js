b92404aa752602ba27b86bfe2e36761b79272cea
jerzyn <andrew.jarzyna@gmail.com>
2015-11-11 13:02:45 +0100

    Change listen port to 8080

--- index.js
+++ index.js
@@ -65,6 +65,6 @@ app.get('/register.csv', function (req, res) {
 
 
 
-app.listen(443, function () {
-  console.log('Server listening on', 443);
+app.listen(8080, function () {
+  console.log('Server listening on', 8080);
 });
